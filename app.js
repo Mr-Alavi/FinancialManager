ccf6b4e8ae4ff0b86fe7fe0a86d73da7f3b6c81a
Mr.Alavi <x.mralavi.x@gmail.com>
2026-07-19 04:57:38 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-Commit new file
+let balance = 0;
+let income = 0;
+let expense = 0;
+
+
+function update(){
+
+document.getElementById("balance").innerText =
+balance.toLocaleString()+" تومان";
+
+document.getElementById("income").innerText =
+income.toLocaleString()+" تومان";
+
+document.getElementById("expense").innerText =
+expense.toLocaleString()+" تومان";
+
+}
+
+
+document.querySelector(".main-btn")
+.onclick = function(){
+
+let type = prompt(
+"نوع تراکنش:\n1 درآمد\n2 خرج"
+);
+
+
+let amount = Number(
+prompt("مبلغ را وارد کنید")
+);
+
+
+if(type==="1"){
+
+income += amount;
+balance += amount;
+
+}
+
+else if(type==="2"){
+
+expense += amount;
+balance -= amount;
+
+}
+
+
+update();
+
+}
+
+
+update();
