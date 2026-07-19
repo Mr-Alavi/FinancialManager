e24566cdf9b5ccbcc793dc2b3c32ef4cbc6f96f0
Mr.Alavi <x.mralavi.x@gmail.com>
2026-07-19 04:58:38 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,54 @@
-let balance = 0;
-let income = 0;
-let expense = 0;
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+
+function save(){
+    localStorage.setItem(
+        "transactions",
+        JSON.stringify(transactions)
+    );
+}
 
 
 function update(){
 
-document.getElementById("balance").innerText =
-balance.toLocaleString()+" تومان";
+    let balance = 0;
+    let income = 0;
+    let expense = 0;
 
-document.getElementById("income").innerText =
-income.toLocaleString()+" تومان";
 
-document.getElementById("expense").innerText =
-expense.toLocaleString()+" تومان";
+    transactions.forEach(t => {
+
+        if(t.type === "income"){
+            income += t.amount;
+            balance += t.amount;
+        }
+
+        else{
+            expense += t.amount;
+            balance -= t.amount;
+        }
+
+    });
+
+
+    document.getElementById("balance").innerText =
+    balance.toLocaleString()+" تومان";
+
+
+    document.getElementById("income").innerText =
+    income.toLocaleString()+" تومان";
+
+
+    document.getElementById("expense").innerText =
+    expense.toLocaleString()+" تومان";
 
 }
 
 
+
 document.querySelector(".main-btn")
 .onclick = function(){
+
 
 let type = prompt(
 "نوع تراکنش:\n1 درآمد\n2 خرج"
@@ -26,28 +56,33 @@ let type = prompt(
 
 
 let amount = Number(
-prompt("مبلغ را وارد کنید")
+prompt("مبلغ:")
 );
 
 
-if(type==="1"){
+if(!amount) return;
 
-income += amount;
-balance += amount;
 
-}
+transactions.push({
 
-else if(type==="2"){
+type: type === "1" ? "income" : "expense",
 
-expense += amount;
-balance -= amount;
+amount: amount,
 
-}
+date: new Date().toLocaleDateString("fa-IR")
 
+});
+
+
+save();
 
 update();
 
+
+alert("✅ ثبت شد");
+
 }
+
 
 
 update();
