7cf4a61a906b5f61af1ecf91620e7eae578397bc
Mr.Alavi <x.mralavi.x@gmail.com>
2026-07-21 01:25:39 +0330

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-
-// Service Worker Registration
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js');
-}
-
-console.log("Financial Manager initialized for Mr. Alavi");
