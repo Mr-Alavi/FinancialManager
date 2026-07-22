938546c991f6fe9a118c102fb922afea1eb9ef83
Mr.Alavi <x.mralavi.x@gmail.com>
2026-07-22 20:53:19 +0330

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'tror-financial-v1.0.0';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './css/variables.css',
+  './css/global.css',
+  './themes/dark.css',
+  './themes/light.css',
+  './js/app.js',
+  './js/router.js',
+  './shared/constants/config.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
